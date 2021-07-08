df0a16f80e49e6487c8a2271bd21090b25b0252b
nataliep769 <nataliep769@gmail.com>
2021-07-08 13:17:27 -0600

    Removed unused imports

--- src/test/java/stepdefinitions/YouTubeHomePageStepDefinitions.java
+++ src/test/java/stepdefinitions/YouTubeHomePageStepDefinitions.java
@@ -1,26 +1,12 @@
 package stepdefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import page.YouTubeHomePage;
-
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class YouTubeHomePageStepDefinitions {
 
-    private YouTubeHomePage youTubeHomePage;
+    private final YouTubeHomePage youTubeHomePage;
 
     public YouTubeHomePageStepDefinitions(YouTubeHomePage youTubeHomePage) {
         this.youTubeHomePage = youTubeHomePage;
