aa7cf7cb4779671caecdb683dd746f56a111935c
nataliep769 <nataliep769@gmail.com>
2021-07-12 14:49:14 -0600

    Removed unused webdriverwait

--- src/test/java/stepdefinitions/IMDBResultsPageComponentSteps.java
+++ src/test/java/stepdefinitions/IMDBResultsPageComponentSteps.java
@@ -18,11 +18,9 @@ import static org.hamcrest.Matchers.containsString;
 public class IMDBResultsPageComponentSteps {
 
     private final IMDBResultsPage imdbResultsPage;
-    private final WebDriverWait webDriverWait;
 
     public IMDBResultsPageComponentSteps(WebDriverManager webDriverManager) {
         this.imdbResultsPage = new IMDBResultsPage(webDriverManager.getWebDriver());
-        this.webDriverWait = new WebDriverWait(webDriverManager.getWebDriver(), 5);
     }
 
     @Then("I will see movies with titles that include Batman")
